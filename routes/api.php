c439c5f137a8483ad85bc09b7c89055161349c5b
iustin.ciobanu <iustinciobanu1995@gmail.com>
2021-12-22 11:22:32 +0200

    Add Review Controller

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('categories', 'Api\CategoryController@index');
-Route::get('products', 'Api\ProductController@index');
+Route::get('products', [ProductController::class, 'getAll']);
